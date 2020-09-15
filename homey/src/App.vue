370ba8d8c65f8fe42513e324d2a9b1e01ddf2d96
Vincent Kempers <me@vncn.tk>
2020-09-15 08:52:16 +0200

    making todo app but dev tools broke

--- homey/src/App.vue
+++ homey/src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <Todos />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id:1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id:2,
+          title: "Todo Two",
+          completed: false
+        },
+        {
+          id:3,
+          title: "Todo Three",
+          completed: true
+        },
+      ]
+    }
   }
 }
 </script>
