973ba5f42828f37a7e6b065468b990d165dd9417
Vincent Kempers <me@vncn.tk>
2020-09-26 13:24:44 +0200

    small changes in header

---
+++ homey/components/molecules/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+      <nav>
+        <h1>B-movie experience</h1>
+        <div class="socials">
+          <ul>
+            <li>About</li>
+            <li>MOTW</li>
+            <li>search component</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+}
+
+nav {
+  display: flex;
+  flex-direction: row;
+}
+</style>
