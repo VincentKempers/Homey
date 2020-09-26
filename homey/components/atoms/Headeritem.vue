051a4ad8e66165044f1b435f4c0e1ad0b6837758
Vincent Kempers <me@vncn.tk>
2020-09-26 15:28:07 +0200

    add headeritem to atoms

---
+++ homey/components/atoms/Headeritem.vue
@@ -0,0 +1,48 @@
+<template>
+  <router-link :to="linkForButton">
+    <button class="headerItem--button">
+      {{ textForButton }}
+    </button>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    textForButton: {
+      type: String,
+      default: 'something'
+    },
+    linkForButton: {
+      type: String,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .headerItem {
+    border: none;
+    font-size: 1rem;
+    width: auto;
+    height: auto;
+  }
+
+  .headerItem--button {
+    background-color: rgba(0,0,0,0.4);
+    color:white;
+    font-size: 1rem;
+    font-weight: 600;
+    border: none;
+    border-radius: .7rem;
+    padding: 1rem;
+    transition: background-color .4s ease-in;
+  }
+
+  .headerItem--button:hover {
+    background-color: rgba(0,0,0,0.6);
+    transition: background-color .2s ease-in;
+  }
+
+</style>
