03633ac89c1ce263506884febbbcad2439112acc
Vincent Kempers <me@vncn.tk>
2020-09-26 14:45:40 +0200

    add styleguides

---
+++ homey/components/styleguide/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="styleguide">
+    <section class="style--atoms">
+      <h2>Atoms</h2>
+      <p>Here you can find all the atoms</p>
+      <div class="atoms--items">
+        <Logo />
+      </div>
+    </section>
+    <section class="style--molecules">
+      <h2>Molecules</h2>
+      <p>Here you can find all the Molecules</p>
+      <div class="molecules--items">
+        <Header />
+      </div>
+    </section>
+    <section class="style--organisms">
+      <h2>Organisms</h2>
+      <p>Here you can find all the Organisms</p>
+      <div class="organisms--items">
+        <p>Nothing yet</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// =============== ATOMS ===============
+import Logo from '../atoms/Logo'
+
+// =============== Molecules ===============
+import Header from '../molecules/Header'
+
+// =============== ORGANISMS ===============
+
+export default {
+  components: {
+    Logo,
+    Header
+  }
+}
+</script>
+
+<style scoped>
+  .styleguide > section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    margin: 4rem 0rem;
+  }
+
+  .atoms--items {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-right: 2rem;
+  }
+
+  .molecules--items, .organisms--items {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:left;
+    margin: 2rem 0;
+  }
+</style>
---
+++ homey/pages/styleguide/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="styleguide--container">
+    <h1>Here you will find all the components</h1>
+    <Styleguide />
+  </div>
+</template>
+
+<script>
+import Styleguide from '../../components/styleguide'
+export default {
+  Styleguide
+}
+</script>
+
+<style scoped>
+
+</style>
