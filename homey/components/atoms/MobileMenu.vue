45db29f123aa9daad29659d685d029b7d51d1e7d
Vincent Kempers <me@vncn.tk>
2020-09-28 14:14:25 +0200

    add new buttons

---
+++ homey/components/atoms/MobileMenu.vue
@@ -0,0 +1,76 @@
+<template>
+  <a href="/" tabindex="0">
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+  </a>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  a {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 4rem;
+    height: 4rem;
+    border: 0.3rem solid #000;
+    background-color: black;
+    transition: all .6s ease-in-out;
+  }
+
+  a:hover, a:active, a:focus {
+    background-color:white;
+    border: 0.3rem solid gold;
+    transition: all .4s ease-in-out;
+  }
+
+  a:hover > .line,
+  a:focus > .line,
+  a:active > .line {
+    border-bottom: 0.3rem solid gold;
+    transition: all 1s ease-in;
+  }
+
+  .line {
+    width: 2rem;
+    height: 0.2rem;
+    border-bottom: 0.19rem solid white;
+    margin-top: .2rem;
+    position: relative;
+    margin-bottom: .2rem;
+    opacity: 1;
+    transform: rotate(0deg);
+    transition: all .4s ease-in;
+  }
+
+  a:hover > .line:first-of-type,
+  a:focus > .line:first-of-type,
+  a:active > .line:first-of-type {
+    position: absolute;
+    transform: rotate(45deg);
+    transition: all .3s ease-in;
+  }
+
+  a:hover > .line:nth-child(2),
+  a:focus > .line:nth-child(2),
+  a:active > .line:nth-child(2) {
+    opacity: 0;
+    transition: all .1s ease-in;
+  }
+
+  a:hover > .line:last-of-type,
+  a:focus > .line:last-of-type,
+  a:active > .line:last-of-type {
+    position: absolute;
+    transform: rotate(-45deg);
+    transition: all .3s ease-in;
+  }
+
+</style>
---
+++ homey/components/atoms/ShinyButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="shiny--button">
+    very shiny
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .shiny--button {
+    background: orange;
+    color: #fff;
+    font-size: 1rem;
+    border-radius: 0.5em;
+    padding: .5rem 2rem;
+    border: none;
+    position: relative;
+    overflow: hidden;
+    line-height: 32px;
+  }
+
+  .shiny--button::after {
+    content: '';
+    position: absolute;
+    top: -50%;
+    right: -50%;
+    bottom: -50%;
+    left: -50%;
+    background: linear-gradient(to bottom, rgba(229, 172, 142, 0), rgba(255,255,255,0.5) 50%, rgba(229, 172, 142, 0));
+    transform: rotateZ(60deg) translate(-5em, 7.5em);
+  }
+
+  button:hover::after, button:focus::after {
+    animation: sheen 1s ease;
+  }
+
+  @keyframes sheen {
+  100% {
+      transform: rotateZ(60deg) translate(1em, -9em);
+    }
+  }
+</style>
